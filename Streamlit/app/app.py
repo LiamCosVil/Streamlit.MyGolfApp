49d53598b99f23a77afcbc6d37e475c2e09dd3c5
LiamCosVil <74625974+LiamCosVil@users.noreply.github.com>
2024-09-05 20:27:49 +0200

    Update app.py

--- Streamlit/app/app.py
+++ Streamlit/app/app.py
@@ -5,7 +5,7 @@ st.markdown(" <style> div[class^='css-1544g2n'] { padding-top: 1rem; } </style> 
 #st.markdown(" <style> div[class^='block-container'] { padding-top: 1rem; } </style> ", unsafe_allow_html=True)
 
 pg = st.navigation([
-    st.Page("Home.py", title="Home"),
+    st.Page("Homey.py", title="Home"),
     st.Page("ViewSG.py", title="ViewSG"),
     st.Page("AddRound\AddRound.py", title="AddRound"),
     st.Page("AddRound\AddRoundTest.py", title="AddRoundTest"),
