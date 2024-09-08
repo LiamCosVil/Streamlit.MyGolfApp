119cade7e35c50f3196cb31ca9270c9f4b01c78e
LiamCosVil <74625974+LiamCosVil@users.noreply.github.com>
2024-09-08 16:47:01 +0200

    Update Homey.py

--- Streamlit/app/Homey.py
+++ Streamlit/app/Homey.py
@@ -1,9 +1,13 @@
+# streamlit_app.py
+
 import streamlit as st
+from st_files_connection import FilesConnection
 
-st.title("Home")
+# Create connection object and retrieve file contents.
+# Specify input format is a csv and to cache the result for 600 seconds.
+conn = st.connection('gcs', type=FilesConnection)
+df = conn.read("streamlit-bucket/myfile.csv", input_format="csv", ttl=600)
 
-st.write("Hello world")
-
-age = st.slider("How old are you?", 0, 130, 0)
-print(age)
-st.write("I'm ", age, "years old")
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.Owner} has a :{row.Pet}:")
