a101410aef8f835741e84242a99833eab7fc59cc
LiamCosVil <74625974+LiamCosVil@users.noreply.github.com>
2024-09-11 17:09:00 +0200

    Update Homey.py

--- Streamlit/app/Homey.py
+++ Streamlit/app/Homey.py
@@ -6,11 +6,11 @@ from st_files_connection import FilesConnection
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
 conn = st.connection("myGCDB", type="sql")
-
+connOld = st.connection("MYSG", type="sql")
 
 # Perform query.
-df = conn.query('SELECT * FROM home;', ttl="10m")
+df = connOld.query('SELECT * FROM Clubs;')
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+if st.button("Press to do"):
+    for index, row in df.iterrows():
+        conn.query("INSERT INTO Clubs VALUES (" + str(row["Club ID"]) + ",'" + str(row["Club Name"]) + "')")
