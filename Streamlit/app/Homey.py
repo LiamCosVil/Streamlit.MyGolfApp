5d722684021640f8e7a08b3985536adf9e27e459
LiamCosVil <74625974+LiamCosVil@users.noreply.github.com>
2024-09-11 17:12:18 +0200

    Update Homey.py

--- Streamlit/app/Homey.py
+++ Streamlit/app/Homey.py
@@ -9,8 +9,8 @@ conn = st.connection("myGCDB", type="sql")
 connOld = st.connection("MYSG", type="sql")
 
 # Perform query.
-df = connOld.query('SELECT * FROM Clubs;')
-
+dfOld = connOld.query('SELECT * FROM Clubs;')
+df = conn.query('SELECT * FROM Clubs;')
 if st.button("Press to do"):
-    for index, row in df.iterrows():
-        conn.query("INSERT INTO Clubs VALUES (" + str(row["Club ID"]) + ",'" + str(row["Club Name"]) + "')")
+    print(dfOld)
+    print(df)
