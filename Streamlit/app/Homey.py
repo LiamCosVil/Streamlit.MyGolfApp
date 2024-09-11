8630304c9221d855ddd45cdc6539355aa61cfa32
LiamCosVil <74625974+LiamCosVil@users.noreply.github.com>
2024-09-11 17:21:00 +0200

    Update Homey.py

--- Streamlit/app/Homey.py
+++ Streamlit/app/Homey.py
@@ -9,17 +9,4 @@ conn = st.connection("myGCDB", type="sql")
 connOld = st.connection("MYSG", type="sql")
 
 # Perform query.
-dfOld = connOld.query('SELECT * FROM Clubs;')
-df = conn.query('SELECT * FROM Clubs;')
-
-print(df)
-print(dfOld)
-
-for index, row in dfOld.iterrows():
-    st.write("INSERT INTO Clubs VALUES (" + str(row["Club ID"]) + ",'" + str(row["Club Name"]) + "')")
-    #conn.query("INSERT INTO Clubs VALUES (" + str(row["Club ID"]) + ",'" + str(row["Club Name"]) + "')")
-
-st.write("NEXT")
-
-for index, row in df.iterrows():
-    st.write("INSERT INTO Clubs VALUES (" + str(row["Club ID"]) + ",'" + str(row["Club Name"]) + "')")
+conn.query("CREATE TABLE TestClubs ('Club ID' SERIAL PRIMARY KEY 'Club Name' VARCHAR(100));")
