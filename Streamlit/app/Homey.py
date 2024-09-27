054a3ce690ab1d191309fd238f51c82a5582e253
LiamCosVil <74625974+LiamCosVil@users.noreply.github.com>
2024-09-27 10:38:17 +0200

    Update Homey.py

--- Streamlit/app/Homey.py
+++ Streamlit/app/Homey.py
@@ -5,19 +5,9 @@ from st_files_connection import FilesConnection
 
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
-conn = st.connection("myGCDB", type="sql")
-connOld = st.connection("MYSG", type="sql")
+conn = st.connection('HomeDB', type='sql')
 
-# Perform query.
-dfOld = connOld.query('SELECT * FROM Clubs;')
-df = conn.query('SELECT * FROM Clubs;')
-
-
-conn.query("CREATE TABLE TestClubs (ID , Name);")
-
-conn.query("INSERT INTO TestClubs (ID, Name) VALUES (0, 'Driver')")
-
-df = conn.query("'SELECT * FROM Clubs;")
+df = conn.query('SELECT * from mytable;', ttl=600)
 
 for index, row in df.iterrows():
     st.write(str(row["ID"]) + str(row["Name"]))
