814380debb00a254bab38578a06be32446efd332
LiamCosVil <74625974+LiamCosVil@users.noreply.github.com>
2024-09-27 10:41:09 +0200

    Update Homey.py

--- Streamlit/app/Homey.py
+++ Streamlit/app/Homey.py
@@ -1,7 +1,7 @@
 # streamlit_app.py
 
 import streamlit as st
-from st_files_connection import FilesConnection
+# from st_files_connection import FilesConnection
 
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
