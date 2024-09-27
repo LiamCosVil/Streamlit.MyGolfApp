8d6cb2a73a4303a45ac739a7c2ff23b6c851bb64
LiamCosVil <74625974+LiamCosVil@users.noreply.github.com>
2024-09-27 12:03:36 +0200

    Update Homey.py

--- Streamlit/app/Homey.py
+++ Streamlit/app/Homey.py
@@ -14,5 +14,5 @@ for index, row in df.iterrows():
 
 if st.button("DO THE THING"):
     with conn.session as s:
-        s.execute("INSERT INTO Test_Table (colorID, color) VALUES (2, 'Blue') RETURNING colorID;")
+        s.execute(text("INSERT INTO Test_Table (colorID, color) VALUES (2, 'Blue') RETURNING colorID;"))
         s.commit()
