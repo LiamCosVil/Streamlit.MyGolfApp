3d005589caa41f42288bc084d5af16d9666e1b9a
LiamCosVil <74625974+LiamCosVil@users.noreply.github.com>
2024-09-28 18:47:43 +0200

    Update app.py

--- Streamlit/app/app.py
+++ Streamlit/app/app.py
@@ -6,10 +6,10 @@ st.markdown(" <style> div[class^='css-1544g2n'] { padding-top: 1rem; } </style> 
 
 pg = st.navigation([
     st.Page("Home.py", title="Home"),
-    st.Page("SG\ViewSG.py", title="ViewSG"),
-    st.Page("AddRound\AddRound.py", title="AddRound"),
-    st.Page("AddRound\AddRoundTest.py", title="AddRoundTest"),
-    st.Page("Stats\ViewStats.py", title="ViewStats"),
+    st.Page("SG/ViewSG.py", title="ViewSG"),
+    st.Page("AddRound/AddRound.py", title="AddRound"),
+    st.Page("AddRound/AddRoundTest.py", title="AddRoundTest"),
+    st.Page("Stats/ViewStats.py", title="ViewStats"),
 ])
 pg.run()
 
