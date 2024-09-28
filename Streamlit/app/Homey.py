46d3a2b4a44290263b69837c5cd788e7de0508ed
LiamCosVil <74625974+LiamCosVil@users.noreply.github.com>
2024-09-28 15:58:59 +0200

    Update Homey.py

--- Streamlit/app/Homey.py
+++ Streamlit/app/Homey.py
@@ -10,7 +10,7 @@ import os
 conn = st.connection('HomeDB', type='sql')
 connLoc = st.connection('MYSG', type='sql')
 
-df = connLoc.query('SELECT * from Rounds;', ttl=0)
+df = connLoc.query('SELECT * from Holes;', ttl=0)
 os.write(1, f"{df}\n".encode()) 
 
 
@@ -18,22 +18,23 @@ os.write(1, f"{df}\n".encode())
 if st.button("DO THE THING"):
     for index, row in df.iterrows():
         with conn.session as s:
-            s.execute(text("""INSERT INTO Rounds
-                            (Round_ID, Player_ID, Total_Score, Total_Par, Holes_Played, Tees_Played, Course_ID, Competition,
-                            Weather, Wind, Date, Score2Par) 
+            s.execute(text("""INSERT INTO Holes
+                            (Hole_ID, Round_ID, Hole_Number, Played_As, Hole_Par, Hole_Score, Hole_Handycap, GIR,
+                            Up_D, Fairway_OOT, Bunker_Up_D, Putts, Hole_Length) 
                             VALUES 
-                            ("""+str(row["Round ID"])+""", 
-                            """+str(row["Player ID"])+""", 
-                            """+str(row["Total Score"])+""", 
-                            """+str(row["Total Par"])+""", 
-                            """+str(row["Holes Played"])+""",
-                            '"""+str(row["Tees Played"])+"""',
-                            """+str(row["Course Played"])+""",
-                            """+str(row["Competition?"])+""",
-                            '"""+str(row["Weather"])+"""',
-                            '"""+str(row["Wind"])+"""',
-                            '"""+str(row["Date"])+"""',
-                            '"""+str(row["Score2Par"])+"""'
+                            ("""+str(row["Hole ID"])+""", 
+                            """+str(row["Round ID"])+""", 
+                            """+str(row["Hole Number"])+""", 
+                            """+str(row["Played As"])+""", 
+                            """+str(row["Hole Par"])+""",
+                            """+str(row["Hole Score"])+""",
+                            """+str(row["Hole Handycap"])+""",
+                            """+str(row["GIR"])+""",
+                            """+str(row["UP&D"])+""",
+                            """+str(row["Fairway OTT"])+""",
+                            """+str(row["Bunker UP&D"])+""",
+                            """+str(row["Hole Length"])+""",
+                            """+str(row["Putts"])+"""
                             );"""))
             s.commit()
             os.write(1, f"{index}\n".encode()) 
